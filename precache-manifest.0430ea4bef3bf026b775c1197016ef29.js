dec5ba038c0b47e99241f0c5a37cb01105dd6b56
Ericko Wicaksono <ericko.wicaksono@gmail.com>
2018-10-18 18:26:25 +0700

    Updates

---
+++ precache-manifest.0430ea4bef3bf026b775c1197016ef29.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1bc0e48e54fb84dfd3e5efbfdea3b75",
+    "url": "/opreker-page-main-forum/static/media/dummy.a1bc0e48.jpg"
+  },
+  {
+    "revision": "70dda1f5652f003b511d",
+    "url": "/opreker-page-main-forum/static/css/main.2cf21213.chunk.css"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/opreker-page-main-forum/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "715199ef72ffdbdb8af5",
+    "url": "/opreker-page-main-forum/static/js/1.715199ef.chunk.js"
+  },
+  {
+    "revision": "1ac4534c50b15668446b",
+    "url": "/opreker-page-main-forum/static/js/runtime~main.1ac4534c.js"
+  },
+  {
+    "revision": "c92b85a5b907c70211f4ec25e29a8c4a",
+    "url": "/opreker-page-main-forum/static/media/favicon.c92b85a5.ico"
+  },
+  {
+    "revision": "70dda1f5652f003b511d",
+    "url": "/opreker-page-main-forum/static/js/main.70dda1f5.chunk.js"
+  },
+  {
+    "revision": "914ad2d8ef0fa4d2eb0226ba375cb55c",
+    "url": "/opreker-page-main-forum/static/media/dummy1.914ad2d8.jpeg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/opreker-page-main-forum/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/opreker-page-main-forum/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/opreker-page-main-forum/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "715199ef72ffdbdb8af5",
+    "url": "/opreker-page-main-forum/static/css/1.aa98db8d.chunk.css"
+  },
+  {
+    "revision": "ab3e6ab5d9ff43c0f96784e8338c5846",
+    "url": "/opreker-page-main-forum/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1dfd28ef904d4562cc048fca2beb5109.js"
+  "/opreker-page-main-forum/precache-manifest.0430ea4bef3bf026b775c1197016ef29.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/opreker-page-main-forum/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
